50c57a7a010dad17c97e5b26d2a59a96812d3134
vishvagohil34 <145643729+vishvagohil34@users.noreply.github.com>
2023-10-05 19:28:53 +0530

    Add files via upload

---
+++ Lab 10/swichday.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main(){
+	int n;
+	printf("enter number : ");
+	scanf("%d",&n);
+	switch(n){
+		case 1:
+		printf("monday");
+		break;
+		case 2:
+		printf("tuesday");
+		break;
+		case 3:
+		printf("wednesday");
+		break;
+		case 4:
+		printf("thursday");
+		break;
+		case 5:
+		printf("friday");
+		break;
+		case 6:
+		printf("saturday");
+		break;
+		case 7:
+		printf("sunday");
+		break;
+		defult:
+		printf("invalid number");
+	}
+}
